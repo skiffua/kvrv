96578566e13d71ef6781f7244cf32e58247aec4f
Oleh <oleh_mushora@ukr.net>
2018-09-03 19:16:23 +0000

    initial

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var rez=document.getElementById('result');
-var mainimage=document.getElementById('mainpic');
-
+let rez=document.getElementById('result');
 
 function checkIfNumber(ev) {
     let data=ev.key;
     let element=ev.target.getAttribute("name");
-
     if (!isNaN(data)) {
-        console.log('chuslo');
+        console.log('number');
     } else {
         let warning=`<span>У поле <b>${element}</b> потрібно ввести число!</span>`;
         let myinput=ev.target;
@@ -17,35 +14,39 @@ function checkIfNumber(ev) {
     }
 }
 
-function kvrn() {
-    let answer;
+function getherData(){
     let a=document.querySelector('[name="a"]').value;
     let b=document.querySelector('[name="b"]').value;
     let c=document.querySelector('[name="c"]').value;
-    a=+a; b=+b; c=+c;
-    console.log(a,b,c);
+    let mainimage=document.getElementById('mainpic');
+        a=parseFloat(a);
+        b=parseFloat(b);
+        c=parseFloat(c);
     if (!isNaN(a) && !isNaN(b) && !isNaN(c) && a!==0) {
-        let d=b*b-4*a*c;
-        if (d<0) {
-            answer='Дискримінант від\'ємний. Рівняння не має розв\'язків';
-            rez.innerHTML=answer;
-        } else {
-            mainimage.setAttribute('src','math_teacher.jpg');
-            let x1 = (-b + Math.sqrt(d)) / (2 * a);
-            let x2 = (-b - Math.sqrt(d)) / (2 * a);
-            answer = `x1=${Math.round(x1*100)/100}  x2=${Math.round(x2*100)/100}`;
-            rez.innerHTML = answer;
-        }
+        rez.innerHTML=solveQuadraticEquation(a,b,c);
+        mainimage.setAttribute('src','math_teacher.jpg');
     } else {
-        answer=`<p style="color: red">У всі поля потрібно ввести числа!</p>`;
-        rez.innerHTML = answer;
+        rez.innerHTML = `<p style="color: red">У всі поля потрібно ввести числа!</p>`;
         mainimage.setAttribute('src','angry-teacher.jpg');
         hideWarning();
     }
 }
 
+function solveQuadraticEquation(a,b,c) {
+        let answer;
+        console.log(a,b,c);
+        let d=b**2-4*a*c;
+        if (d<0) {
+            answer='Дискримінант від\'ємний. Рівняння не має розв\'язків';
+            return answer;
+        } else {
+            let x1 = (-b + Math.sqrt(d)) / (2 * a);
+            let x2 = (-b - Math.sqrt(d)) / (2 * a);
+            answer = `x1=${Math.round(x1*100)/100}  x2=${Math.round(x2*100)/100}`;
+            return answer;
+        }
+}
 
 function hideWarning() {
     setTimeout("rez.innerHTML=''",3000);
-
 }
